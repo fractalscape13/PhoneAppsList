9df947d6ca9aee521881655cd0dac24788832a77
Joseph Wangemann <fractalscape13@gmail.com>
2020-03-16 15:24:35 -0700

    finish save method for app.cs

--- PhoneApps/Models/App.cs
+++ PhoneApps/Models/App.cs
@@ -25,6 +25,32 @@ namespace PhoneApps.Models
       MySqlConnection conn = DB.Connection();
       conn.Open();
       var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO apps (name, developer, category, releaseDate) VALUES (@AppName, @AppDeveloper, @AppCategory, @AppReleaseDate);";
+      MySqlParameter name = new MySqlParameter();
+      MySqlParameter developer = new MySqlParameter();
+      MySqlParameter category = new MySqlParameter();
+      MySqlParameter releaseDate = new MySqlParameter();
+      name.ParameterName = "@AppName";
+      developer.ParameterName = "@AppDeveloper";
+      category.ParameterName = "@AppCategory";
+      releaseDate.ParameterName = "@AppReleaseDate";
+      name.Value = this.Name;
+      developer.Value = this.Developer;
+      category.Value = this.Category;
+      releaseDate.Value = this.ReleaseDate;
+      cmd.Parameters.Add(name);
+      cmd.Parameters.Add(developer);
+      cmd.Parameters.Add(category);
+      cmd.Parameters.Add(releaseDate);
+
+      cmd.ExecuteNonQuery();
+      Id = (int)cmd.LastInsertedId;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
     }
   }
 }
